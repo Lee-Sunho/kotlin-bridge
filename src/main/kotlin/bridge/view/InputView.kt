bd15f5c5b07fefa15fa5ee012c38bcec275a6bb4
Lee-Sunho <sunho9889@soongsil.ac.kr>
2022-12-16 22:12:11 +0900

    feat(InputView) : 이동할 칸 예외 처리 메소드 구현

--- src/main/kotlin/bridge/view/InputView.kt
+++ src/main/kotlin/bridge/view/InputView.kt
@@ -1,10 +1,17 @@
 package bridge.view
 
+import bridge.constants.Constant.Companion.MAX_BRIDGE_LENGTH
+import bridge.constants.Constant.Companion.MIN_BRIDGE_LENGTH
+import bridge.constants.Exception.Companion.EXCEPTION_INVALID_MOVING
+import bridge.constants.Exception.Companion.EXCEPTION_INVALID_RANGE_NUMBER
+import bridge.constants.Exception.Companion.EXCEPTION_INVALID_RETRY
+import bridge.constants.Exception.Companion.EXCEPTION_IS_NOT_NUMBER
+import bridge.constants.String.Companion.STRING_DOWN
+import bridge.constants.String.Companion.STRING_QUIT
+import bridge.constants.String.Companion.STRING_RETRY
+import bridge.constants.String.Companion.STRING_UP
 import camp.nextstep.edu.missionutils.Console
 
-/**
- * 사용자로부터 입력을 받는 역할을 한다.
- */
 class InputView {
     private val outputView = OutputView()
 
@@ -15,21 +22,47 @@ class InputView {
     fun readBridgeSize(): Int {
         outputView.printInputBridgeLength()
         val length = Console.readLine()
-        // 유효성 검사
+        checkBridgeLength(length)
         return length.toInt()
+    }
+
+    private fun checkBridgeLength(length: String) {
+        validateIsNumber(length)
+        validateNumberRange(length)
+    }
+
+    private fun validateIsNumber(input: String) {
+        if (!input.all { Character.isDigit(it) }) {
+            outputView.printException(EXCEPTION_IS_NOT_NUMBER)
+            throw  IllegalArgumentException(EXCEPTION_IS_NOT_NUMBER)
+        }
+    }
+    private fun validateNumberRange(input: String) {
+        if (input.toInt() < MIN_BRIDGE_LENGTH || input.toInt() > MAX_BRIDGE_LENGTH) {
+            outputView.printException(EXCEPTION_INVALID_RANGE_NUMBER)
+            throw  IllegalArgumentException(EXCEPTION_INVALID_RANGE_NUMBER)
+        }
     }
 
     fun readMoving(): String {
         outputView.printInputMoving()
         val moving = Console.readLine()
-        // 유효성 검사
+        checkMoving(moving)
         return moving
+    }
+
+    private fun checkMoving(moving: String) {
+        val valid = listOf<String>(STRING_UP, STRING_DOWN)
+        if (!valid.contains(moving)) {
+            outputView.printException(EXCEPTION_INVALID_MOVING)
+            throw  IllegalArgumentException(EXCEPTION_INVALID_MOVING)
+        }
     }
 
     fun readGameCommand(): String {
         outputView.printInputRetry()
         val retry = Console.readLine()
-        // 유효성 검사
+        //checkRetry(retry)
         return retry
     }
 }
